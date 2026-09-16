76421e33a29e88ce3fb22b9414ac3825567814e1
Calabriel <87156643+Saerkyne@users.noreply.github.com>
2026-09-16 11:12:11 -0400

    Even more instructions, whew boy

--- cpu.cpp
+++ cpu.cpp
@@ -23,7 +23,6 @@ void cpu_reset()
 
 void cpu_fetch()
 {
-	// TODO: Read from memory bus instead of directly from ROM data
 	cpu_current_op_code = memory_bus_read(cpu_registers.pc++);
 	const gb_cpu_instruction& instruction = instructions[cpu_current_op_code];
 	cpu_current_instruction_execute = instruction.execute;
@@ -59,6 +58,16 @@ void cpu_ld_bc_a() // 0x02
 	cpu_routine_ld_ptr8(cpu_registers.bc, cpu_registers.a); // Load 8-bit value into a from 16-bit memory address in BC
 }
 
+void cpu_inc_bc() // 0x03
+{
+	cpu_routine_inc_16(cpu_registers.bc); // Increment BC register pair
+}
+
+void cpu_inc_b() // 0x04
+{
+	cpu_routine_inc_8(cpu_registers.b); // Increment B register
+}
+
 void cpu_dec_b() // 0x05
 {
 	cpu_routine_dec_8(cpu_registers.b); // Decrement B register
@@ -69,14 +78,34 @@ void cpu_ld_b_n() // 0x06
 	cpu_routine_ld_8(cpu_registers.b); // Load 8-bit immediate value into B register
 }
 
-void cpu_ld_c_n() // 0x0E
+void cpu_add_hl_bc() // 0x09
 {
-	cpu_routine_ld_8(cpu_registers.c); // Load 8-bit immediate value into C register
+	cpu_routine_add_hl_16(cpu_registers.bc); // Add BC register pair to HL register pair
+}
+
+void cpu_ld_a_bc() // 0x0A
+{
+	cpu_routine_ld_ptr16(cpu_registers.a, cpu_registers.bc); // Load 8-bit value into A from 16-bit memory address in BC
+}
+
+void cpu_dec_bc() // 0x0B
+{
+	cpu_routine_dec_16(cpu_registers.bc); // Decrement BC register pair
+}
+
+void cpu_inc_c() // 0x0C
+{
+	cpu_routine_inc_8(cpu_registers.c); // Increment C register
 }
 
 void cpu_dec_c() // 0x0D
 {
 	cpu_routine_dec_8(cpu_registers.c); // Decrement C register
+}
+
+void cpu_ld_c_n() // 0x0E
+{
+	cpu_routine_ld_8(cpu_registers.c); // Load 8-bit immediate value into C register
 }
 
 void cpu_ld_de_nn() // 0x11
@@ -89,6 +118,16 @@ void cpu_ld_de_a() // 0x12
 	cpu_routine_ld_ptr8(cpu_registers.de, cpu_registers.a); // Load 8-bit value into register a from 16-bit memory address in DE
 }
 
+void cpu_inc_de() // 0x13
+{
+	cpu_routine_inc_16(cpu_registers.de); // Increment DE register pair
+}
+
+void cpu_inc_d() // 0x14
+{
+	cpu_routine_inc_8(cpu_registers.d); // Increment D register
+}
+
 void cpu_dec_d() // 0x15
 {
 	cpu_routine_dec_8(cpu_registers.d); // Decrement D register
@@ -97,6 +136,26 @@ void cpu_dec_d() // 0x15
 void cpu_ld_d_n() // 0x16
 {
 	cpu_routine_ld_8(cpu_registers.d); // Load 8-bit immediate value into D register
+}
+
+void cpu_add_hl_de() // 0x19
+{
+	cpu_routine_add_hl_16(cpu_registers.de); // Add DE register pair to HL register pair
+}
+
+void cpu_ld_a_de() // 0x1A
+{
+	cpu_routine_ld_ptr16(cpu_registers.a, cpu_registers.de); // Load 8-bit value into A from 16-bit memory address in DE
+}
+
+void cpu_dec_de() // 0x1B
+{
+	cpu_routine_dec_16(cpu_registers.de); // Decrement DE register pair
+}
+
+void cpu_inc_e() // 0x1C
+{
+	cpu_routine_inc_8(cpu_registers.e); // Increment E register
 }
 
 void cpu_dec_e() // 0x1D
@@ -114,6 +173,16 @@ void cpu_ld_hl_nn() // 0x21
 	cpu_routine_ld_16(cpu_registers.h, cpu_registers.l); // Load 16-bit immediate value into HL register pair
 }
 
+void cpu_inc_hl() // 0x23
+{
+	cpu_routine_inc_16(cpu_registers.hl); // Increment HL register pair
+}
+
+void cpu_inc_h() // 0x24
+{
+	cpu_routine_inc_8(cpu_registers.h); // Increment H register
+}
+
 void cpu_dec_h() // 0x25
 {
 	cpu_routine_dec_8(cpu_registers.h); // Decrement H register
@@ -122,6 +191,16 @@ void cpu_dec_h() // 0x25
 void cpu_ld_h_n() // 0x26
 {
 	cpu_routine_ld_8(cpu_registers.h); // Load 8-bit immediate value into H register
+}
+
+void cpu_dec_hl() // 0x2B
+{
+	cpu_routine_dec_16(cpu_registers.hl); // Decrement HL register pair
+}
+
+void cpu_inc_l() // 0x2C
+{
+	cpu_routine_inc_8(cpu_registers.l); // Increment L register
 }
 
 void cpu_dec_l() // 0x2D
@@ -147,6 +226,26 @@ void cpu_ldd_hl_a() // 0x32
 	core_advance_cpu_clocks(4);
 }
 
+void cpu_inc_sp() // 0x33
+{
+	cpu_routine_inc_16(cpu_registers.sp); // Increment SP register
+}
+
+void cpu_add_hl_sp() // 0x39
+{
+	cpu_routine_add_hl_16(cpu_registers.sp); // Add SP register to HL register pair
+}
+
+void cpu_dec_sp() // 0x3B
+{
+	cpu_routine_dec_16(cpu_registers.sp); // Decrement SP register
+}
+
+void cpu_inc_a() // 0x3C
+{
+	cpu_routine_inc_8(cpu_registers.a); // Increment A register
+}
+
 void cpu_dec_a() // 0x3D
 {
 	cpu_routine_dec_8(cpu_registers.a); // Decrement A register
@@ -155,6 +254,41 @@ void cpu_dec_a() // 0x3D
 void cpu_ld_a_n() // 0x3E
 {
 	cpu_routine_ld_8(cpu_registers.a); // Load 8-bit immediate value into A register
+}
+
+void cpu_ld_b_hl() // 0x46
+{
+	cpu_routine_ld_ptr16(cpu_registers.b, cpu_registers.hl); // Load 8-bit value into B from 16-bit memory address in HL
+}
+
+void cpu_ld_c_hl() // 0x4E
+{
+	cpu_routine_ld_ptr16(cpu_registers.c, cpu_registers.hl); // Load 8-bit value into C from 16-bit memory address in HL
+}
+
+void cpu_ld_d_hl() // 0x56
+{
+	cpu_routine_ld_ptr16(cpu_registers.d, cpu_registers.hl); // Load 8-bit value into D from 16-bit memory address in HL
+}
+
+void cpu_ld_e_hl() // 0x5E
+{
+	cpu_routine_ld_ptr16(cpu_registers.e, cpu_registers.hl); // Load 8-bit value into E from 16-bit memory address in HL
+}
+
+void cpu_ld_h_hl() // 0x66
+{
+	cpu_routine_ld_ptr16(cpu_registers.h, cpu_registers.hl); // Load 8-bit value into H from 16-bit memory address in HL
+}
+
+void cpu_ld_l_hl() // 0x6E
+{
+	cpu_routine_ld_ptr16(cpu_registers.l, cpu_registers.hl); // Load 8-bit value into L from 16-bit memory address in HL
+}
+
+void cpu_ld_a_hl() // 0x7E
+{
+	cpu_routine_ld_ptr16(cpu_registers.a, cpu_registers.hl); // Load 8-bit value into A from 16-bit memory address in HL
 }
 
 void cpu_jp_nn() // 0xC3
@@ -182,6 +316,90 @@ void cpu_ld_hl_c() // 0x71
 	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.c); // Load 8-bit value into register C from 16-bit memory address in HL
 }
 
+void cpu_ld_hl_d() // 0x72
+{
+	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.d); // Load 8-bit value into register D from 16-bit memory address in HL
+}
+
+void cpu_ld_hl_e() // 0x73
+{
+	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.e); // Load 8-bit value into register E from 16-bit memory address in HL
+}
+
+void cpu_ld_hl_h() // 0x74
+{
+	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.h); // Load 8-bit value into register H from 16-bit memory address in HL
+}
+
+void cpu_ld_hl_l() // 0x75
+{
+	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.l); // Load 8-bit value into register L from 16-bit memory address in HL
+}
+
+void cpu_ld_hl_a() // 0x77
+{
+	cpu_routine_ld_ptr8(cpu_registers.hl, cpu_registers.a); // Load 8-bit value into register A from 16-bit memory address in HL
+}
+
+void cpu_add_a_b() // 0x80
+{
+	cpu_routine_add_a_8(cpu_registers.b); // Add B register to A register
+}
+
+void cpu_add_a_c() // 0x81
+{
+	cpu_routine_add_a_8(cpu_registers.c); // Add C register to A register
+}
+
+void cpu_add_a_d() // 0x82
+{
+	cpu_routine_add_a_8(cpu_registers.d); // Add D register to A register
+}
+
+void cpu_add_a_e() // 0x83
+{
+	cpu_routine_add_a_8(cpu_registers.e); // Add E register to A register
+}
+
+void cpu_add_a_h() // 0x84
+{
+	cpu_routine_add_a_8(cpu_registers.h); // Add H register to A register
+}
+
+void cpu_add_a_l() // 0x85
+{
+	cpu_routine_add_a_8(cpu_registers.l); // Add L register to A register
+}
+
+void cpu_adc_a_b() // 0x88
+{
+	cpu_routine_adc_a_8(cpu_registers.b); // Add B register and carry flag to A register
+}
+
+void cpu_adc_a_c() // 0x89
+{
+	cpu_routine_adc_a_8(cpu_registers.c); // Add C register and carry flag to A register
+}
+
+void cpu_adc_a_d() // 0x8A
+{
+	cpu_routine_adc_a_8(cpu_registers.d); // Add D register and carry flag to A register
+}
+
+void cpu_adc_a_e() // 0x8B
+{
+	cpu_routine_adc_a_8(cpu_registers.e); // Add E register and carry flag to A register
+}
+
+void cpu_adc_a_h() // 0x8C
+{
+	cpu_routine_adc_a_8(cpu_registers.h); // Add H register and carry flag to A register
+}
+
+void cpu_adc_a_l() // 0x8D
+{
+	cpu_routine_adc_a_8(cpu_registers.l); // Add L register and carry flag to A register
+}
 
 void cpu_xor_a() // 0xAF
 {
--- cpu_instructions.cpp
+++ cpu_instructions.cpp
@@ -5,64 +5,64 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "NOP",			0,		cpu_noop		},	// 0x00
 	{ "LD BC, nn",		2,		cpu_ld_bc_nn	},	// 0x01
 	{ "LD (BC), A",		0,		cpu_ld_bc_a		},	// 0x02
-	{ "INC BC",			0,		nullptr			},	// 0x03
-	{ "INC B",			0,		nullptr			},	// 0x04
+	{ "INC BC",			0,		cpu_inc_bc		},	// 0x03
+	{ "INC B",			0,		cpu_inc_b		},	// 0x04
 	{ "DEC B",			0,		cpu_dec_b		},	// 0x05
 	{ "LD B, n",		1,		cpu_ld_b_n		},	// 0x06
 	{ "RLC A",			0,		nullptr			},	// 0x07
 	{ "LD nn, SP",		2,		nullptr			},	// 0x08
-	{ "ADD HL, BC",		0,		nullptr			},	// 0x09
-	{ "LD A, (BC)",		0,		nullptr			},	// 0x0A
-	{ "DEC BC",			0,		nullptr			},	// 0x0B
-	{ "INC C",			0,		nullptr			},	// 0x0C
+	{ "ADD HL, BC",		0,		cpu_add_hl_bc	},	// 0x09
+	{ "LD A, (BC)",		0,		cpu_ld_a_bc		},	// 0x0A
+	{ "DEC BC",			0,		cpu_dec_bc		},	// 0x0B
+	{ "INC C",			0,		cpu_inc_c		},	// 0x0C
 	{ "DEC C",			0,		cpu_dec_c		},	// 0x0D
 	{ "LD C, n",		1,		cpu_ld_c_n		},	// 0x0E
 	{ "RRC A",			0,		nullptr			},	// 0x0F
 	{ "STOP",			1,		nullptr			},	// 0x10
 	{ "LD DE, nn",		2,		cpu_ld_de_nn	},	// 0x11
 	{ "LD (DE), A",		0,		cpu_ld_de_a		},	// 0x12
-	{ "INC DE",			0, 		nullptr 		},	// 0x13
-	{ "INC D",			0,		nullptr			},	// 0x14
+	{ "INC DE",			0, 		cpu_inc_de 		},	// 0x13
+	{ "INC D",			0,		cpu_inc_d		},	// 0x14
 	{ "DEC D",			0,		cpu_dec_d		},	// 0x15
 	{ "LD D, n",		1,		cpu_ld_d_n		},	// 0x16
 	{ "RLA",			0,		nullptr			},	// 0x17
 	{ "JR n",			1,		nullptr			},	// 0x18
-	{ "ADD HL, DE",		0,		nullptr			},	// 0x19
-	{ "LD A, (DE)",		0,		nullptr			},	// 0x1A
-	{ "DEC DE",			0,		nullptr			},	// 0x1B
-	{ "INC E",			0,		nullptr			},	// 0x1C
+	{ "ADD HL, DE",		0,		cpu_add_hl_de	},	// 0x19
+	{ "LD A, (DE)",		0,		cpu_ld_a_de		},	// 0x1A
+	{ "DEC DE",			0,		cpu_dec_de		},	// 0x1B
+	{ "INC E",			0,		cpu_inc_e		},	// 0x1C
 	{ "DEC E",			0,		cpu_dec_e		},	// 0x1D
 	{ "LD E, n",		1,		cpu_ld_e_n		},	// 0x1E
 	{ "RRA",			0,		nullptr			},	// 0x1F
 	{ "JR NZ, n",		1,		nullptr			},	// 0x20
 	{ "LD HL, nn",		2,		cpu_ld_hl_nn	},	// 0x21
 	{ "LD (HL+), A",	0,		nullptr			},	// 0x22
-	{ "INC HL",			0,		nullptr			},	// 0x23
-	{ "INC H",			0,		nullptr			},	// 0x24
+	{ "INC HL",			0,		cpu_inc_hl		},	// 0x23
+	{ "INC H",			0,		cpu_inc_h		},	// 0x24
 	{ "DEC H",			0,		cpu_dec_h		},	// 0x25
 	{ "LD H, n",		1,		cpu_ld_h_n		},	// 0x26
 	{ "DAA",			0,		nullptr			},	// 0x27
 	{ "JR Z, n",		1,		nullptr			},	// 0x28
 	{ "ADD HL, HL",		0,		nullptr			},	// 0x29
 	{ "LD A, (HL+)",	0,		nullptr			},	// 0x2A
-	{ "DEC HL",			0,		nullptr			},	// 0x2B
-	{ "INC L",			0,		nullptr			},	// 0x2C
+	{ "DEC HL",			0,		cpu_dec_hl		},	// 0x2B
+	{ "INC L",			0,		cpu_inc_l		},	// 0x2C
 	{ "DEC L",			0,		cpu_dec_l		},	// 0x2D
 	{ "LD L, n",		1,		cpu_ld_l_n		},	// 0x2E
 	{ "CPL",			0,		nullptr			},	// 0x2F
 	{ "JR NC, n",		1,		nullptr			},	// 0x30
 	{ "LD SP, nn",		2,		cpu_ld_sp_nn	},	// 0x31
 	{ "LD (HL-), A",	0,		cpu_ldd_hl_a	},	// 0x32
-	{ "INC SP",			0,		nullptr			},	// 0x33
+	{ "INC SP",			0,		cpu_inc_sp		},	// 0x33
 	{ "INC (HL)",		0,		nullptr			},	// 0x34
 	{ "DEC (HL)",		0,		nullptr			},	// 0x35
 	{ "LD (HL), n",		1,		nullptr			},	// 0x36
 	{ "SCF",			0,		nullptr			},	// 0x37
 	{ "JR C, n",		1, 		nullptr 		},	// 0x38
-	{ "ADD HL, SP",		0,		nullptr			},	// 0x39
+	{ "ADD HL, SP",		0,		cpu_add_hl_sp	},	// 0x39
 	{ "LD A, (HL-)",	0,		nullptr			},	// 0x3A
-	{ "DEC SP",			0,		nullptr			},	// 0x3B
-	{ "INC A",			0,		nullptr			},	// 0x3C
+	{ "DEC SP",			0,		cpu_dec_sp		},	// 0x3B
+	{ "INC A",			0,		cpu_inc_a		},	// 0x3C
 	{ "DEC A",			0,		cpu_dec_a		},	// 0x3D
 	{ "LD A, n",		1,		cpu_ld_a_n		},	// 0x3E
 	{ "CCF",			0,		nullptr			},	// 0x3F
@@ -72,7 +72,7 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD B, E",		0,		nullptr			},	// 0x43
 	{ "LD B, H",		0,		nullptr			},	// 0x44
 	{ "LD B, L",		0,		nullptr			},	// 0x45
-	{ "LD B, (HL)",		0,		nullptr			},	// 0x46
+	{ "LD B, (HL)",		0,		cpu_ld_b_hl		},	// 0x46
 	{ "LD B, A",		0,		nullptr			},	// 0x47
 	{ "LD C, B",		0,		nullptr			},	// 0x48
 	{ "LD C, C",		0,		nullptr			},	// 0x49
@@ -80,7 +80,7 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD C, E",		0,		nullptr			},	// 0x4B
 	{ "LD C, H",		0,		nullptr			},	// 0x4C
 	{ "LD C, L",		0,		nullptr			},	// 0x4D
-	{ "LD C, (HL)",		0,		nullptr			},	// 0x4E
+	{ "LD C, (HL)",		0,		cpu_ld_c_hl		},	// 0x4E
 	{ "LD C, A",		0,		nullptr			},	// 0x4F
 	{ "LD D, B",		0, 		nullptr 		},	// 0x50
     { "LD D, C", 		0, 		nullptr 		},	// 0x51
@@ -88,7 +88,7 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD D, E",		0,		nullptr			},	// 0x53
 	{ "LD D, H",		0,		nullptr			},	// 0x54
 	{ "LD D, L",		0,		nullptr			},	// 0x55
-	{ "LD D, (HL)",		0,		nullptr			},	// 0x56
+	{ "LD D, (HL)",		0,		cpu_ld_d_hl		},	// 0x56
 	{ "LD D, A",		0,		nullptr			},	// 0x57
 	{ "LD E, B",		0,		nullptr			},	// 0x58
 	{ "LD E, C",		0,		nullptr			},	// 0x59
@@ -96,7 +96,7 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD E, E",		0,		nullptr			},	// 0x5B
 	{ "LD E, H",		0,		nullptr			},	// 0x5C
 	{ "LD E, L",		0,		nullptr			},	// 0x5D
-	{ "LD E, (HL)",		0,		nullptr			},	// 0x5E
+	{ "LD E, (HL)",		0,		cpu_ld_e_hl		},	// 0x5E
 	{ "LD E, A",		0,		nullptr			},	// 0x5F
 	{ "LD H, B",		0,		nullptr			},	// 0x60
 	{ "LD H, C",		0,		nullptr			},	// 0x61
@@ -104,7 +104,7 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD H, E",		0,		nullptr			},	// 0x63
 	{ "LD H, H",		0,		nullptr			},	// 0x64
 	{ "LD H, L",		0,		nullptr			},	// 0x65
-	{ "LD H, (HL)",		0,		nullptr			},	// 0x66
+	{ "LD H, (HL)",		0,		cpu_ld_h_hl		},	// 0x66
 	{ "LD H, A",		0,		nullptr			},	// 0x67
 	{ "LD L, B",		0,		nullptr			},	// 0x68
 	{ "LD L, C",		0,		nullptr			},	// 0x69
@@ -112,38 +112,38 @@ const struct gb_cpu_instruction instructions[256] = {
 	{ "LD L, E",		0, 		nullptr 		},	// 0x6B
     { "LD L, H", 		0, 		nullptr 		},	// 0x6C
 	{ "LD L, L",		0,		nullptr			},	// 0x6D
-	{ "LD L, (HL)",		0,		nullptr			},	// 0x6E
+	{ "LD L, (HL)",		0,		cpu_ld_l_hl		},	// 0x6E
 	{ "LD L, A",		0,		nullptr			},	// 0x6F
 	{ "LD (HL), B",		0,		cpu_ld_hl_b		},	// 0x70
 	{ "LD (HL), C",		0,		cpu_ld_hl_c		},	// 0x71
-	{ "LD (HL), D",		0,		nullptr			},	// 0x72
-	{ "LD (HL), E",		0,		nullptr			},	// 0x73
-	{ "LD (HL), H",		0,		nullptr			},	// 0x74
-	{ "LD (HL), L",		0,		nullptr			},	// 0x75
+	{ "LD (HL), D",		0,		cpu_ld_hl_d		},	// 0x72
+	{ "LD (HL), E",		0,		cpu_ld_hl_e		},	// 0x73
+	{ "LD (HL), H",		0,		cpu_ld_hl_h		},	// 0x74
+	{ "LD (HL), L",		0,		cpu_ld_hl_l		},	// 0x75
 	{ "HALT",			0,		nullptr			},	// 0x76
-	{ "LD (HL), A",		0,		nullptr			},	// 0x77
+	{ "LD (HL), A",		0,		cpu_ld_hl_a		},	// 0x77
 	{ "LD A, B",		0,		nullptr			},	// 0x78
 	{ "LD A, C",		0,		nullptr			},	// 0x79
 	{ "LD A, D",		0,		nullptr			},	// 0x7A
 	{ "LD A, E",		0,		nullptr			},	// 0x7B
 	{ "LD A, H",		0,		nullptr			},	// 0x7C
 	{ "LD A, L",		0,		nullptr			},	// 0x7D
-	{ "LD A, (HL)",		0,		nullptr			},	// 0x7E
+	{ "LD A, (HL)",		0,		cpu_ld_a_hl		},	// 0x7E
 	{ "LD A, A",		0,		nullptr			},	// 0x7F
-	{ "ADD A, B",		0,		nullptr			},	// 0x80
-	{ "ADD A, C",		0,		nullptr			},	// 0x81
-	{ "ADD A, D",		0,		nullptr			},	// 0x82
-	{ "ADD A, E",		0,		nullptr			},	// 0x83
-	{ "ADD A, H",		0,		nullptr			},	// 0x84
-	{ "ADD A, L",		0,		nullptr			},	// 0x85
+	{ "ADD A, B",		0,		cpu_add_a_b		},	// 0x80
+	{ "ADD A, C",		0,		cpu_add_a_c		},	// 0x81
+	{ "ADD A, D",		0,		cpu_add_a_d		},	// 0x82
+	{ "ADD A, E",		0,		cpu_add_a_e		},	// 0x83
+	{ "ADD A, H",		0,		cpu_add_a_h		},	// 0x84
+	{ "ADD A, L",		0,		cpu_add_a_l		},	// 0x85
 	{ "ADD A, (HL)",	0,		nullptr			},	// 0x86
 	{ "ADD A, A",		0,		nullptr			},	// 0x87
-	{ "ADC A, B",		0,		nullptr			},	// 0x88
-	{ "ADC A, C",		0,		nullptr			},	// 0x89
-	{ "ADC A, D",		0,		nullptr			},	// 0x8A
-	{ "ADC A, E",		0,		nullptr			},	// 0x8B
-	{ "ADC A, H",		0,		nullptr			},	// 0x8C
-	{ "ADC A, L",		0,		nullptr			},	// 0x8D
+	{ "ADC A, B",		0,		cpu_adc_a_b		},	// 0x88
+	{ "ADC A, C",		0,		cpu_adc_a_c		},	// 0x89
+	{ "ADC A, D",		0,		cpu_adc_a_d		},	// 0x8A
+	{ "ADC A, E",		0,		cpu_adc_a_e		},	// 0x8B
+	{ "ADC A, H",		0,		cpu_adc_a_h		},	// 0x8C
+	{ "ADC A, L",		0,		cpu_adc_a_l		},	// 0x8D
 	{ "ADC A, (HL)",	0,		nullptr			},	// 0x8E
 	{ "ADC A, A",		0,		nullptr			},	// 0x8F
 	{ "SUB B",			0,		nullptr			},	// 0x90
